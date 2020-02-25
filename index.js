88e8f3a51fc7f54173bc85a44d6042f7eb3c0ff3
Victoria  Topham <topham.victoria@gmail.com>
2020-02-25 08:58:51 -0800

    did firstNamesAllCaps but with test broken

--- index.js
+++ index.js
@@ -214,8 +214,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const toCaps = runners.map((item) => {
+    return item.first_name.toUpperCase();
+  });
+
+  return toCaps;
 }
 
 /**
